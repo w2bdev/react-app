e19892e42176ea438b615fae0fb2d935e3fc1f79
Wayne Wong <bin5661@gmail.com>
2017-08-26 22:23:44 +0800

    import jquery-ui

--- src/components/Datepicker/Datepicker.js
+++ src/components/Datepicker/Datepicker.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import $ from 'jquery'
+import 'jquery-ui/ui/widgets/datepicker';
 
 class Datepicker extends Component {
     componentDidMount(){
         this.$el = $(this.el)
         this.$el.addClass("blue");
+        this.$el.datepicker();
         
     }
 
